69b0702b720c8703c8884ff094cfabb557669681
soham ingle <inglevaibhavi03@gmail.com>
2023-09-16 11:22:29 +0530

    adding files

---
+++ LseekFile.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+
+int main()
+{
+	char fname[30];
+	int fd = 0;
+	char Data[11];
+	
+	printf("Enter the file name that you want to open\n");
+	scanf("%s",fname);
+	
+	fd = open(fname,O_RDWR);
+	if(fd == -1)
+	{
+		printf("Unable to open file\n");
+		return -1;
+	}
+	else 
+	{
+		printf("File is succesfully opened with fd : %d\n",fd);
+	}
+	
+	lseek(fd,-7,2);
+	
+	read(fd,Data,6);
+	
+	write(1,Data,6);
+	
+	close(fd);
+	
+	return 0;
+}
